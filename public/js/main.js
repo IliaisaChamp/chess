7f7ddd74adf10e6701521793246095d91cdb82f8
SSK <sskolcov@gmail.com>
2021-10-27 18:43:20 +0300

    release modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-console.log('ok');
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("123");
+  const elems = document.querySelectorAll('.modal');
+  const instances = M.Modal.init(elems);
+});
+
