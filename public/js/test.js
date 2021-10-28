eb2f2a1d1ab6c92cfaf052244b7e7dcc18e7dc1a
Aleksandr Shman <sancho1741@mail.ru>
2021-10-28 17:01:12 +0300

    release test v2.0

--- public/js/test.js
+++ public/js/test.js
@@ -1,53 +1,12 @@
-// // console.log('ok')
-
-// document.addEventListener('submit', async (event) => {
-//   event.preventDefault();
-//   console.log(event.target);
-//   if (event.target.name === 'addForm') {
-//     $formWraper = document.querySelector('[data-formWraper]');
-//     $formWraper.insertAdjacentHTML('beforeend', formAddTest());
-
-//     event.target.remove();
-//   } if (event.target.name === 'addFormHorse') {
-//     const newHorse = Object.fromEntries(new FormData(event.target));
-//     // console.log( newHorse );
-//     console.log(newHorse);
-//     const response = await fetch('/horses', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(newHorse),
-//     });
-//     if (response.ok) {
-//       const dataFromBack = await response.json();
-//       // console.log(dataFromBack);
-//       const { newHorse: { name } } = dataFromBack;
-//       // console.log({name});
-//     }
-//   }
-//   // event.target.remove()
-
-//   // $SubmitButton = document.querySelector('SubmitButton')
-//   // console.log($SubmitButton);
-//   // $SubmitButton?.insertAdjacentHTML('beforeend', buttonAddaHorse())
-// });
-
-// function buttonAddaHorse() {
-//     return `<form name= "addForm">
-//     <button type="submit" class="button dark">Add a Horse</button>
-//     </form>`;
-// }
-
 document.addEventListener('click', async (event) => {
-    event.preventDefault();
-    if (event.target.name === 'buttonTestAdd') {
-        $delliteButton = document.querySelector('#buttonTest')
-        $formWraper = document.querySelector('[data-tabWrapper]');
-        $formWraper.insertAdjacentHTML('beforeend', formAddtest());
-       $delliteButton.remove()
-    }
-    });
+  event.preventDefault();
+  if (event.target.name === 'buttonTestAdd') {
+    $delliteButton = document.querySelector('#buttonTest');
+    $formWraper = document.querySelector('[data-tabWrapper]');
+    $formWraper.insertAdjacentHTML('beforeend', formAddtest());
+    $delliteButton.remove();
+  }
+});
 function formAddtest() {
   return `<div style="margin-top: 3%" class="row">
   <div class="col s8 offset-s2">
